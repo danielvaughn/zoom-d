027b1591f25dda97fc219133c3979f5e827a8183
Daniel Vaughn <danielbvaughn@gmail.com>
2015-01-18 19:35:40 -0500

    Initial commit - setting up grunt/folder structure

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# zoom-d
-A simple jQuery plugin for allowing zoom on a designated image
+# Zoom-d
+
+A simple plugin for image zooming.
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/danielvaughn/zoom-d/master/dist/zoom-d.min.js
+[max]: https://raw.github.com/danielvaughn/zoom-d/master/dist/zoom-d.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/zoom-d.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/jquery.zoom-d.js
@@ -0,0 +1,33 @@
+/*
+ * zoom-d
+ * https://github.com/danielvaughn/zoom-d
+ *
+ * Copyright (c) 2015 Daniel Vaughn
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.zoomd = function() {
+    return this.each(function(i) {
+      // Return an initial log for each selected element.
+      window.console.log('test: ' + i);
+      window.console.log(this);
+    });
+  };
+
+  // Static method.
+  $.zoomd = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.zoomd.options, options);
+    // Return something awesome.
+    return 'test: ' + options.max;
+  };
+
+  // Static method default options.
+  $.zoomd.options = {
+    max: 5 // X times greater than native image resolution
+  };
+
+}(jQuery));
